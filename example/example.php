07984036b1b836ca2f5557fca057ccc12ba4f451
Jason <Jason>
2023-12-22 18:25:21 +0800

    fix

--- example/example.php
+++ example/example.php
@@ -24,5 +24,18 @@ if (is_file(__DIR__ . '/../vendor/autoload.php')) {
 Process::init('testjobs', __DIR__)
     ->registerJob(new TestJob1())
     ->registerJob(new TestJob2())
+    ->registerAlarmHook(function (string $message) {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, 'https://oapi.dingtalk.com/robot/send?access_token=******');
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 5);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, ['Content-Type: application/json;charset=utf-8']);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode(['msgtype' => 'text', 'text' => ['content' => 'PREFIX:' . $message]]));
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_exec($ch);
+        curl_close($ch);
+    })
     ->execute($argv[1] ?? '');
 
